4f0e49b8aec35193dbec39891e7ad54730a629e9
Андрей Ковалев <kovalev.andy@yandex.ru>
2025-04-12 14:28:12 +0300

    common_search fix response error

--- tg_bot/bot/common_search/search.py
+++ tg_bot/bot/common_search/search.py
@@ -1,19 +1,23 @@
 import logging
+from http.client import OK
 
 import requests
-
 from requests.exceptions import ConnectionError
 
 from settings import AZM_COMMON_SEARCH_URL
 
 
 async def search(search_text: str):
+    result_list = []
     url = ''.join((AZM_COMMON_SEARCH_URL, '/search'))
     try:
         response = requests.get(url, {"text": search_text})
-        result_list = response.json()
+        if response.status_code == OK:
+            result_list = response.json()
+        else:
+            logging.critical(
+                f'Ошибка запроса к AZM_COMMON_SEARCH {url}\nStatusCode {str(response.status_code)}: {response.text}')
     except ConnectionError as e:
-        logging.critical(f'Ошибка подключения к AZM_COMMON_SEARCH {url}')
-        result_list = []
+        logging.critical(f'Ошибка подключения к AZM_COMMON_SEARCH {url}\n{str(e)}')
 
     return result_list
