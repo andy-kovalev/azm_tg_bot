6d48767cc4a3f755228d8221e25fbb22f5409521
Андрей Ковалев <kovalev.andy@yandex.ru>
2025-04-29 21:04:10 +0300

    fix get_user_info

--- tg_bot/bot/utils/user.py
+++ tg_bot/bot/utils/user.py
@@ -3,4 +3,9 @@ from aiogram.types import Message
 
 async def get_user_info(message: Message) -> dict:
     user = message.from_user
-    return {'id': str(user.id), 'username': user.username, 'name': ' '.join((user.first_name, user.last_name)).strip()}
+    _id = user.id if user.id else 0
+    _username = user.username if user.username else '-'
+    _first_name = user.first_name if user.first_name else '-'
+    _last_name = user.last_name if user.last_name else '-'
+
+    return {'id': str(_id), 'username': _username, 'name': ' '.join((_first_name, _last_name)).strip()}
